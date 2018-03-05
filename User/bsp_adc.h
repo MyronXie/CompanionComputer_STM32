65b77a30c364dae0612f313946119449818dff7a
Mingye Xie <mingye.xie@clobotics.com>
2018-03-05 11:19:39 +0800

    Build basic architecture of ADC

---
+++ User/bsp_adc.h
@@ -0,0 +1,26 @@
+/**
+  ******************************************************************************
+  * File Name       : bsp_adc.h
+  * Description     : Drivers for ADC (based on HAL)
+  *
+  * Version         : v0.2
+  * Created Date    : 2018.03.05
+  * Revised Date    : 2018.03.05
+  *
+  * Author          : Mingye Xie
+  ******************************************************************************
+  */
+
+#ifndef __BSP_ADC_H
+#define __BSP_ADC_H
+
+
+#include "stm32f3xx_hal.h"
+
+
+void ADC_Init(void);
+void DMA_Init(void);
+
+#endif /* __BSP_ADC_H */
+
+/******************************END OF FILE******************************/
